1be07ec78634fa1f9a2aa08298fda54afbc89b3d
dabriz1024 <dbriz1024@gmail.com>
2019-10-02 13:49:07 -0400

    Bootcamp3 Finished

--- Bootcamp3/server/controllers/listings.server.controller.js
+++ Bootcamp3/server/controllers/listings.server.controller.js
@@ -58,10 +58,31 @@ exports.update = function(req, res) {
   var listing = req.listing;
 
   /* Replace the listings's properties with the new properties found in req.body */
- 
+  
   /*save the coordinates (located in req.results if there is an address property) */
- 
+  
   /* Save the listing */
+
+  listing.code = req.body.code;
+  listing.name = req.body.name;
+  listing.address = req.body.address;
+
+  if(req.results){
+    listing.coordinates = {
+      latitude: req.results.lat,
+      longitude: req.results.lng
+    };
+  }
+
+  listing.save(function(err){
+    if(err){
+      console.log(err);
+      res.status(400).send(err);
+    }else{
+      res.json(listing);
+      console.log(listing)
+    }
+  });
 
 };
 
@@ -70,13 +91,33 @@ exports.delete = function(req, res) {
   var listing = req.listing;
 
   /* Add your code to remove the listins */
+  listing.remove(function(err){
+    if(err){
+      res.status(400).send(err);
+      console.log("Error Delete Failed ", err);
+      return;
+    }
+    else{
+      res.end();
+      console.log("Delete Successful");
+    }
+  })
 
 };
 
 /* Retreive all the directory listings, sorted alphabetically by listing code */
 exports.list = function(req, res) {
   /* Add your code */
-};
+  Listing.find({}, function(err,listing){
+    if(err){
+      console.log(err);
+      res.status(400).send(err);
+    } else{
+      res.json(listing);
+      console.log(listing)
+    }
+  });
+}
 
 /* 
   Middleware: find a listing by its ID, then pass it to the next request handler. 
